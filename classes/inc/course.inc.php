efe7d51a0566a120072abeec5e84932aac49565d
b82a9a13 <85174237+b82a9a13@users.noreply.github.com>
2023-09-18 13:39:32 +0100

    0.1.1
    Change Log:
    - Boiler plate code completed

---
+++ classes/inc/course.inc.php
@@ -0,0 +1,6 @@
+<?php
+require_once(__DIR__.'/../../../../config.php');
+use block_iqa\lib;
+$lib = new lib();
+$returnText = new stdClass();
+$p = 'block_iqa';
---
+++ classes/lib.php
@@ -0,0 +1,11 @@
+<?php 
+/**
+ * @package   block_iqa
+ * @author    Robert Tyrone Cullen
+ * @var stdClass $plugin
+ */
+namespace block_iqa;
+
+class lib{
+
+}
